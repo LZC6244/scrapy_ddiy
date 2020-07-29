42a3df1b43c394cb2558393af91a25c0c76011cd
LZC6244 <624486877@qq.com>
2020-07-29 21:12:22 +0800

    完善项目，增加MongoDB管道

---
+++ scrapy_ddiy/pipelines/mongodb.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+MongoDB Pipeline
+"""
+
+import logging
+import pymongo
+from itemadapter import ItemAdapter
+from pymongo.errors import BulkWriteError
+
+logger = logging.getLogger(__name__)
+
+
+class MongodbPipeline(object):
+    mongo_coll: pymongo.collection.Collection
+
+    def __init__(self, mongo_uri, mongo_db, mongo_params, bulk_insert):
+        self.mongo_db = mongo_db
+        self.bulk_insert = bulk_insert
+        self.mongo_cli = pymongo.MongoClient(mongo_uri, **mongo_params)
+        # 用于临时保存数据的列表
+        self.data_li = []
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=crawler.settings.get('MONGO_URI'),
+            mongo_db=crawler.settings.get('MONGO_DATABASE', 'scrapy_ddiy_test'),
+            mongo_params=crawler.settings.getdict('MONGO_PARAMS', {}),
+            bulk_insert=crawler.settings.getint('BULK_INSERT', 5)
+        )
+
+    def open_spider(self, spider):
+        mongo_coll = getattr(spider, 'table_name_mongo', None) or \
+                     getattr(spider, 'table_name_ddiy', None) or \
+                     spider.name
+        self.mongo_coll = self.mongo_cli[self.mongo_db][mongo_coll]
+
+    def close_spider(self, spider):
+        if self.data_li:
+            self.insert_data(spider, self.data_li)
+        self.mongo_cli.close()
+
+    def insert_data(self, spider, data_li: list):
+        try:
+            self.mongo_coll.insert_many(data_li, ordered=False)
+        except BulkWriteError as e:
+            # 插入重复数据（_id）时会报此错误
+            pass
+        logger.info(f'Bulk insert {len(data_li)} items successfully')
+
+    def process_item(self, item, spider):
+        data = ItemAdapter(item).asdict()
+        self.data_li.append(data)
+        if len(self.data_li) >= self.bulk_insert:
+            self.insert_data(spider, self.data_li)
+        return item
--- scrapy_ddiy/utils/spiders/ddiy_base.py
+++ scrapy_ddiy/utils/spiders/ddiy_base.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 import os
+import redis
 import scrapy
-from time import sleep
+import pymongo
 from datetime import datetime
+from scrapy.item import Item, Field
+from scrapy_ddiy.utils.common import get_request_md5
 
 """
 scrapy_ddiy 基本爬虫
@@ -12,7 +15,12 @@ scrapy_ddiy 基本爬虫
 class DdiyBaseSpider(scrapy.Spider):
     name = 'ddiy_base'
 
-    # allowed_domains = ['base']
+    # 管道配置
+    # 数据库表名
+    table_name_ddiy = None
+    # 数据库表名 for MongoDB
+    table_name_mongo = None
+
     start_urls = ['https://www.baidu.com/']
 
     # custom_settings 优先级高于 ddiy_settings
@@ -20,32 +28,53 @@ class DdiyBaseSpider(scrapy.Spider):
         'CONCURRENT_REQUESTS': 1,
         'DOWNLOAD_DELAY': 1,
     }
+    # 预设给爬虫使用的 Redis 连接（如 scrapy_redis 、 记录某些特定数据）
+    redis_cli: redis.Redis
+    # 预设给爬虫使用的 MongoDB 连接（如记录爬虫异常信息）
+    mongo_cli: pymongo.MongoClient
 
     @classmethod
     def update_settings(cls, settings):
         now = datetime.now().strftime('%Y-%m-%dT%H_%M_%S')
         log_file = settings.get('LOG_FILE') or f'spider_logs/{cls.name}/{cls.name}__{now}__{os.getpid()}.log'
-        # log_file = f'spider_logs/{cls.name}/{cls.name}__{now}__{os.getpid()}.log'
         make_log_dir(log_file)
         settings.setdict({'LOG_FILE': log_file}, priority='spider')
         settings.setdict(cls.ddiy_settings or {}, priority='spider')
         settings.setdict(cls.custom_settings or {}, priority='spider')
 
     def custom_init(self, *args, **kwargs):
-        self.logger.info('h' * 30)
-        # self.make_log_dir(self.settings.get('LOG_FILE'))
+        pass
+
+    def base_init(self, *args, **kwargs):
+        if hasattr(self, 'server'):
+            self.redis_cli = self.server
+        else:
+            self.redis_cli = redis.Redis(host=self.settings.get('REDIS_HOST'), port=self.settings.get('REDIS_PORT'),
+                                         **self.settings.getdict('REDIS_PARAMS'))
+        self.mongo_cli = pymongo.MongoClient(self.settings.get('MONGO_URI'), **self.settings.getdict('MONGO_PARAMS'))
+        self.mongo_cli = pymongo.MongoClient(self.settings.get('MONGO_URI'), **self.settings.getdict('MONGO_PARAMS'))
 
     @classmethod
     def from_crawler(cls, crawler, *args, **kwargs):
         spider = super().from_crawler(crawler, *args, **kwargs)
+        spider.base_init(*args, **kwargs)
         spider.custom_init(*args, **kwargs)
         return spider
 
-    def parse(self, response):
-        self.logger.info('hello scrapy.')
-        sleep(1.5)
-        self.logger.warning('hhhhhhhhh')
-        print(response.request.headers['User-Agent'])
+    @staticmethod
+    def _adjust_item(parsed_item: dict):
+        item = Item()
+        for k, v in parsed_item.items():
+            item.fields[k] = Field()
+            item[k] = v
+        return item
+
+    def process_parsed_item(self, response, parsed_item: dict, set_id=True):
+        if set_id:
+            # 默认不覆盖 parsed_item 中带过来的 _id
+            parsed_item.setdefault('_id', get_request_md5(response.request))
+        parsed_item['crawl_time'] = datetime.now()
+        return self._adjust_item(parsed_item)
 
 
 def make_log_dir(log_file: str = None):
