4dc35cd06fc92df66037e67d8a05c748cfe5be42
LZC6244 <624486877@qq.com>
2020-09-01 20:04:19 +0800

    修复 MAKE_LOG_FILE=False 且 LOG_TO_CONSOLE=True 时控制台打印两次日志的问题

---
+++ scrapy_ddiy/downloadermiddlewares/custom_user_agent.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+爬虫 settings 未配置 User-Agent 时 ，为每个请求配置随机请求头
+若已配置，则使用配置的 User-Agent
+"""
+import random
+from scrapy.downloadermiddlewares.useragent import UserAgentMiddleware
+
+USER_AGENTS = [
+    'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 '
+    'Safari/534.50',
+    'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
+    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0',
+    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)',
+    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)',
+    'Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36',
+    'Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; en) Presto/2.8.131 Version/11.11',
+    'Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11',
+    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; The World)',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko'
+]
+
+
+class CustomUserAgent(UserAgentMiddleware):
+
+    def __init__(self, user_agent='Scrapy'):
+        if isinstance(user_agent, str) and 'scrapy' in user_agent.lower():
+            user_agent = ''
+        super().__init__(user_agent)
+
+    def process_request(self, request, spider):
+        if self.user_agent:
+            request.headers.setdefault(b'User-Agent', self.user_agent)
+        else:
+            request.headers.setdefault(b'User-Agent', random.choice(USER_AGENTS))
