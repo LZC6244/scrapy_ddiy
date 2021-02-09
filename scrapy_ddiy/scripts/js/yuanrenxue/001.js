57f216490e4af6b74a999e7ab0141d012b0ce16e
LZC6244 <624486877@qq.com>
2021-02-09 20:29:33 +0800

    格式化代码

--- scrapy_ddiy/scripts/js/yuanrenxue/001.js
+++ scrapy_ddiy/scripts/js/yuanrenxue/001.js
@@ -1,5 +1,5 @@
 const jsdom = require("jsdom");
-const {JSDOM} = jsdom;
+const { JSDOM } = jsdom;
 const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
 window = dom.window;
 document = window.document;
