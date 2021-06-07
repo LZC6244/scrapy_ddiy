a292a4b7cc31585de72fb2bff40ad4d9af68a722
LZC6244 <624486877@qq.com>
2021-06-07 20:12:05 +0800

    update redis_demo_spider

--- scrapy_ddiy/spiders/redis_demo_spider.py
+++ scrapy_ddiy/spiders/redis_demo_spider.py
@@ -7,16 +7,18 @@ scrapy_ddiy 示例爬虫
 """
 
 
-class DemoSpider(DdiyRedisSpider):
+class RedisDemoSpider(DdiyRedisSpider):
     name = 'redis_demo_spider'
     description = 'scrapy_ddiy -  Redis示例爬虫'
-    start_urls = ['https://www.baidu.com/']
     custom_settings = {
         'CONCURRENT_REQUESTS': 1,
         'DOWNLOAD_DELAY': 1,
         'ITEM_PIPELINES': {
             'scrapy_ddiy.pipelines.mongodb.MongodbPipeline': 300,
         },
+        'SCHEDULER': 'scrapy_redis.scheduler.Scheduler',
+        'DUPEFILTER_CLASS': 'scrapy_redis.dupefilter.RFPDupeFilter',
+
         # 'SCHEDULER': 'scrapy_ddiy.utils.scheduler.SchedulerDdiy',
         # 'DUPEFILTER_CLASS': 'scrapy_ddiy.utils.dupefilter.RedisBloomDupeFilter',
         # 'REDIS_BLOOM_CAPACITY': 1000,
@@ -25,10 +27,10 @@ class DemoSpider(DdiyRedisSpider):
 
     def make_request_from_data(self, data):
         demo_url = f'https://www.baidu.com/s?wd={data}'
-        yield Request(url=demo_url, callback=self.parse)
+        yield Request(url=demo_url, callback=self.parse, meta={'dont_redirect': True})
 
-        demo_url_2 = f'https://www.baidu.com/s?wd={data}-{data}'
-        yield Request(url=demo_url_2, callback=self.parse)
+        demo_url_2 = f'https://www.baidu.com/s?wd={data}a{data}'
+        yield Request(url=demo_url_2, callback=self.parse, meta={'dont_redirect': True})
 
     def parse(self, response, **kwargs):
         self.logger.info(f"{response.url} ==> I'm a redis demo spider.")
